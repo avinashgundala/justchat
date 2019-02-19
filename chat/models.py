32f453a96e48a5934a427dd83c1c31777da3a4f3
avinashgundala <gundala.avinash@gmail.com>
2019-02-19 14:57:59 +0530

    first commit

---
+++ chat/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+user = get_user_model()
+
+class Message(models.Model):
+    author = models.ForeignKey(user,related_name ='author_message',on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.author.username
+
+    def last_10_message(self):
+        return Message.objects.order_by(-timestamp).all()[0:10]
